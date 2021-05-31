ae1d164efe29f819c851cb1298ff7696d1356fd3
samod singh <samod0098@gmail.com>
2021-05-31 10:06:17 +0530

    yaahu baba javascript

---
+++ style.js
@@ -0,0 +1,130 @@
+// document.write("functoin in javascript")
+
+// let a= window.document;
+// // a = window.innerHeight;
+// // a = innerWidth;
+// // a = scrollY;
+// // a = location;
+// // console.log(a);
+// // location.href;
+// Array.from(a).forEach(function(Element){a
+//     console.log(Element);
+// }) 
+// a = document.links[0].href;
+// console.log(a)
+
+// function hello (firstname ,scondname){
+//     document.write("Hello"  + firstname+"  "+ scondname +"<br>");
+// }
+// hello("samod","sunil");
+// hello("sudhir singh","umesh kumar");
+
+//   function fullname(math ,english,science){
+//       var a= math+ english +science;
+//       return a;
+//   }
+//   var b =fullname(50,20,40);
+//   document.write(b);
+
+
+// var a = "samod singh";
+// function hello(){
+   
+//     document.write(a + "<br>")
+  
+// }
+// hello();  
+// document.write(a);
+
+//  function hello (){
+//      alert( "Hello everyone");
+
+//  }
+
+//  var a =1;
+//  document.write("<ul>")
+
+//  while(a <=10){
+//      document.write (" <li> " + a +" My name samod singh </li>")
+//      a = a+1;
+//  } 
+//  document.write("</ul>")
+// var a = 1;
+// do{
+//     document.write(a+ ") samod kumar singh <br>");
+//     a++;
+// }while(a <= 10);
+
+// for (var a = 1;a <=10;a++){
+    //     document.write("samod kumar singh <br>")}
+
+
+// for (var a = 1;a <=10;a++){
+//    if(a == 3){
+//        document.write("Hello" + a + "<br>");
+//        break;
+//    }
+
+
+//         document.write(a + "samod kumar singh <br>")
+    
+// }
+// for (var a = 1;a <=30;a++){
+//      if(a % 2 !=0)   {
+//          document.write( a +"<br>");
+//      }
+//     }
+
+// for(var a =1; a <=100;a = a+10){
+// for(var b = a;b <a+10;b++ ){
+// document.write(b + "  ");
+// }
+// document.write("<br>");
+// }
+
+// for(var a = 1;a <=5; a++){
+//     for(var b = 1 ; b <= a;b++){
+//         document.write(b + " ")
+//     } 
+//     document.write("<br>")
+// }
+// for(var a = 1;a <=5; a++){
+//     for(var b = 1 ; b <= a;b++){
+//         document.write(a + " ")
+//     }
+//     document.write("<br>")
+// }
+// for(var a = 5;a >=1; a--){
+//     for(var b = 1 ; b <= a;b++){
+//         document.write(a + " ")
+//     }
+//     document.write("<br>")
+// }
+// for(var a = 5;a >=1; a--){
+//     for(var b = a ; b >= 1;b--){
+//         document.write(b + " ")
+//     }
+//     document.write("<br>")
+// }
+
+// var ary = [20,30,40,50];
+// var sum = 0;
+// for(var a=0;a <=3 ;a++){
+//     document.write(ary [a] + "<br>");
+//  sum =sum + ary [a];
+// }
+// document.write("Total sum :" + sum);
+
+ 
+var ary =new Array ();
+ary[0]=10;
+ary[1]='samod';
+ary[2]=true;
+ary[3]=null;
+ 
+for(var a=0;a <=4 ;a++){
+    document.write(ary [a] + "<br>");
+}
+
+ 
+
